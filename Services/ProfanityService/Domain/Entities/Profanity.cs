a4bff30c351232b103895a7aae75f066cb1e38b5
oliv6362 <33200860+oliv6362@users.noreply.github.com>
2025-09-15 13:43:16 +0200

    refactor(profanity): Added variables to the profanity entity class

--- Services/ProfanityService/Domain/Entities/Profanity.cs
+++ Services/ProfanityService/Domain/Entities/Profanity.cs
@@ -2,4 +2,11 @@ namespace ProfanityService.Domain.Entities;
 public class Profanity
 {
     public int Id { get; set; }
+    public string Term { get; set; }
+    public string NormalizedTerm { get; set; }
+
+    //Simple toggle to check if the term need to be used. 
+    public bool IsActive { get; set; } = true;
+    //How offensive a term is: Mild(0), Moderate(1), Severe(3).
+    public int Serverity { get; set; } = 1;
 }
