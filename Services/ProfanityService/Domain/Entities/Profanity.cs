54a2f5aef8000f1582025231ae2068b52f6ee44a
oliv6362 <33200860+oliv6362@users.noreply.github.com>
2025-09-15 13:44:46 +0200

    style(profanity): removed a space

--- Services/ProfanityService/Domain/Entities/Profanity.cs
+++ Services/ProfanityService/Domain/Entities/Profanity.cs
@@ -4,7 +4,6 @@ public class Profanity
     public int Id { get; set; }
     public string Term { get; set; }
     public string NormalizedTerm { get; set; }
-
     //Simple toggle to check if the term need to be used. 
     public bool IsActive { get; set; } = true;
     //How offensive a term is: Mild(0), Moderate(1), Severe(3).
