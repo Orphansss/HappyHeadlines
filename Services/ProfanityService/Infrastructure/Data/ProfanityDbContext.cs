ad64a3eaf407ed8cfb04eeea6a01e84f54cc4f63
oliv6362 <33200860+oliv6362@users.noreply.github.com>
2025-09-15 14:17:42 +0200

    feat(infrastructure): updated ProfanityDBContext with a new constructor, DbSet and table mappings

--- Services/ProfanityService/Infrastructure/Data/ProfanityDbContext.cs
+++ Services/ProfanityService/Infrastructure/Data/ProfanityDbContext.cs
@@ -1,8 +1,13 @@
 using ProfanityService.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
-namespace ProfanityService.Data;
+namespace ProfanityService.Infrastructure.Data;
 public class ProfanityDbContext : DbContext
 {
-
+    public ProfanityDbContext(DbContextOptions<ProfanityDbContext> options) : base(options) { }
+    public DbSet<Profanity> Profanities => Set<Profanity>();
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Profanity>().ToTable("Profanity");
+    }
 }
