0aa94baa09cbae5fc3a4f304d3ddbffc1920e735
oliv6362 <33200860+oliv6362@users.noreply.github.com>
2025-09-15 16:45:20 +0200

    feat(application): updated IProfanityService with new methods for CRUD operations

--- Services/ProfanityService/Application/Abstractions/IProfanityService.cs
+++ Services/ProfanityService/Application/Abstractions/IProfanityService.cs
@@ -1,7 +1,14 @@
 using ProfanityService.Domain.Entities;
 
-namespace ProfanityService.Abstractions;
+namespace ProfanityService.Application.Abstractions;
 public interface IProfanityService
 {
-
+    Task<Profanity> CreateProfanityTerm(Profanity profanity);
+    Task<IEnumerable<Profanity>> GetProfanityTerms();
+    Task<IEnumerable<Profanity>> GetActiveProfanityTerms(bool includeInactive = false);
+    Task<Profanity?> GetProfanityTerm(int id);
+    Task<Profanity?> UpdateProfanityTerm(int id, Profanity profanity);
+    // Soft Delete terms
+    Task<Boolean> DeactivateProfanityTerm(int id);
+    Task<Boolean> ReactivateProfanityTerm(int id);
 }
