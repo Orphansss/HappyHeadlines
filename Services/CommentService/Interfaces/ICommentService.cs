7f1054eeddccad4ef5f3fab716ca0d469bb6df56
Tobbekjaer <tobias.hougs.kjaer@gmail.com>
2025-09-22 22:47:15 +0200

    Update ICommentService.cs

--- Services/CommentService/Interfaces/ICommentService.cs
+++ Services/CommentService/Interfaces/ICommentService.cs
@@ -4,10 +4,10 @@ namespace CommentService.Interfaces
 {
     public interface ICommentService
     {
-        Task<Comment> CreateComment(Comment comment);
+        Task<Comment> CreateComment(Comment comment, CancellationToken ct);
         Task<IEnumerable<Comment>> GetComments();
         Task<Comment?> GetCommentById(int id);
-        Task<Comment?> UpdateComment(int id, Comment comment);
-        Task<bool> DeleteComment(int id);
+        Task<Comment?> UpdateComment(int id, Comment comment, CancellationToken ct);
+        Task<bool> DeleteComment(int id, CancellationToken ct);
     }
 }
