7e84163bf380f4328ba3081bb272bb3c7e186ee6
Tobbekjaer <tobias.hougs.kjaer@gmail.com>
2025-09-30 10:54:03 +0200

    Added IProfanityClient and RabbitMQ services + Swagger

--- Services/PublisherService/Program.cs
+++ Services/PublisherService/Program.cs
@@ -1,17 +1,37 @@
+using PublisherService.Application.Abstractions;
+using PublisherService.Infrastructure.Messaging;
+using PublisherService.Infrastructure.Profanity;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Options
+builder.Services.Configure<MessagingOptions>(builder.Configuration.GetSection(MessagingOptions.SectionName));
 
+// Profanity HTTP client
+builder.Services
+    .AddHttpClient<IProfanityClient, ProfanityClientHttp>(client =>
+    {
+        var baseUrl = builder.Configuration["PROFANITY_BASEURL"]
+                      ?? throw new InvalidOperationException("PROFANITY_BASEURL not set");
+        client.BaseAddress = new Uri(baseUrl);
+        client.Timeout = TimeSpan.FromSeconds(2);
+    });
+
+// Rabbit publisher
+builder.Services.AddSingleton<IArticleQueuePublisher, ArticleQueuePublisherRabbit>();
+
+// Controllers + Swagger
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
