6389cbe8f08c9e5faeecafa77a77ef15c6b3f36e
Tobbekjaer <tobias.hougs.kjaer@gmail.com>
2025-09-30 14:18:49 +0200

    Update Program.cs

--- Services/PublisherService/Program.cs
+++ Services/PublisherService/Program.cs
@@ -24,6 +24,7 @@ builder.Services
 builder.Services.AddSingleton<IArticleQueuePublisher, ArticleQueuePublisherRabbit>();
 
 builder.Services.AddScoped<PublishArticleHandler>();
+builder.Services.AddSingleton<IIdGenerator, MonotonicIdGenerator>();
 
 // ---- Controllers + Swagger ----
 builder.Services.AddControllers();
