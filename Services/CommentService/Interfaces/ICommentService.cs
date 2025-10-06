daa1330d20625f8e3947c025df3db4aca81c2e09
Tobbekjaer <tobias.hougs.kjaer@gmail.com>
2025-10-06 23:27:04 +0200

    Added GetCommentsForArticle

--- Services/CommentService/Interfaces/ICommentService.cs
+++ Services/CommentService/Interfaces/ICommentService.cs
@@ -4,10 +4,11 @@ namespace CommentService.Interfaces
 {
     public interface ICommentService
     {
-        Task<Comment> CreateComment(Comment comment, CancellationToken ct);
-        Task<IEnumerable<Comment>> GetComments();
-        Task<Comment?> GetCommentById(int id);
-        Task<Comment?> UpdateComment(int id, Comment comment, CancellationToken ct);
-        Task<bool> DeleteComment(int id, CancellationToken ct);
+        Task<Comment> CreateComment(Comment comment, CancellationToken ct = default);
+        Task<IEnumerable<Comment>> GetCommentsForArticle(int articleId, CancellationToken ct = default);
+        Task<Comment?> GetCommentById(int id, CancellationToken ct = default);
+        Task<Comment?> UpdateComment(int id, Comment comment, CancellationToken ct = default);
+        Task<bool> DeleteComment(int id, CancellationToken ct = default);
     }
+
 }
