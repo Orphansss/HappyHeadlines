583a4caceb2734bf2a9a084df90f230f4c9fb2da
oliv6362 <33200860+oliv6362@users.noreply.github.com>
2025-10-09 22:08:13 +0200

    docs(cache): updated comments in articlecache

--- Services/ArticleService/Infrastructure/Caching/ArticleCache.cs
+++ Services/ArticleService/Infrastructure/Caching/ArticleCache.cs
@@ -2,32 +2,26 @@ using System.Text.Json;
 using Microsoft.Extensions.Caching.Distributed;
 using ArticleService.Domain.Entities;
 using ArticleService.Application.Interfaces;
+using StackExchange.Redis;
 
 namespace ArticleService.Infrastructure.Caching;
 
 public sealed class ArticleCache(IDistributedCache cache) : IArticleCache
 {
-    /// <summary>
     /// Gives more options for JsonSerializer
-    /// </summary>
     private static readonly JsonSerializerOptions _json = new(JsonSerializerDefaults.Web)
     {
         WriteIndented = false
     };
 
-    /// <summary>
     /// Cache entry options for a single article or a list of articles.
     /// How long a aricle should live in the cache.
-    /// </summary>
     private static readonly DistributedCacheEntryOptions ItemTtl =
         new() { AbsoluteExpirationRelativeToNow = TimeSpan.FromHours(24) };
 
-
-    /// <summary>
     /// The name of the keys for articles in the cache.
-    /// </summary>
     private static string KeyById(int id) => $"articles:{id}";
-
+ 
     /// <summary>
     /// Get a article from the cache from its Id.
     /// Use JsonSerializer to Deserlize the cached article from bytes to article object.
