0c1aedb07b0ffafe55d6bd1c01aaa5cb3474ad1c
carolinanoemi <cnoemimartinez@gmail.com>
2025-11-08 10:50:16 +0100

    Created ArticleListDto

---
+++ Services/ArticleService/Api/Contracts/Dtos/ArticleListDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ArticleService.Api.Contracts.Dtos
+{
+    public class ArticleListDto
+    {
+        public int Id { get; set; }
+        public int AuthorId { get; set; }
+        public string Title { get; set; }
+        public string? Summary { get; set; }
+        public DateTimeOffset PublishedAt { get; set; } = DateTimeOffset.UtcNow;
+    }
+}
